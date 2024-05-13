299a6d8fbd2916ba06d8d1a45e65d15a01e67a7d
shoyosd <sebastianhoyosduran@gmail.com>
2024-05-12 20:25:42 -0500

    upload project to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,105 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'calculator';
+  pantalla: string = '0';
+  operando1: string = '';
+  operando2: string = '';
+  operacion: string = '';
+  resultado: string = '';
+
+  handleClick(event: any) {
+    const valor = event.target.innerText;
+
+    switch(valor) {
+      case 'C':
+        this.clear();
+        break;
+      case '←':
+        this.borrar();
+        break;
+      case '=':
+        this.calcular();
+        break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        this.establecerOperacion(valor);
+        break;
+      default:
+        this.agregarNumero(valor);
+        break;
+    }
+  }
+
+  clear() {
+    this.pantalla = '0';
+    this.operando1 = '';
+    this.operando2 = '';
+    this.operacion = '';
+    this.resultado = '';
+  }
+
+  borrar() {
+    this.pantalla = this.pantalla.slice(0, -1);
+  }
+
+  establecerOperacion(op: string) {
+    if (this.operando1 === '') {
+      this.operando1 = this.pantalla;
+      this.operacion = op;
+      this.pantalla = '0';
+    } else if (this.operando2 === '') {
+      this.operacion = op;
+    } else {
+      this.calcular();
+      this.operacion = op;
+    }
+  }
+
+  agregarNumero(num: string) {
+    if (this.operacion === '') {
+      // Si no se ha establecido una operación, agregamos el número normalmente
+      this.pantalla === '0' ? this.pantalla = num : this.pantalla += num;
+    } else {
+      // Si se ha establecido una operación, verificamos si estamos ingresando el primer o segundo número
+      if (this.operando2 === '') {
+        // Si es el primer número del segundo operando, lo concatenamos
+        this.operando2 = num;
+        this.pantalla = num;
+      } else {
+        // Si es el segundo dígito del segundo operando, lo concatenamos al número existente
+        this.operando2 += num;
+        this.pantalla = this.operando2;
+      }
+    }
+  }
+  
+
+  calcular() {
+    const num1 = parseFloat(this.operando1);
+    const num2 = parseFloat(this.operando2);
+
+    switch(this.operacion) {
+      case '+':
+        this.resultado = (num1 + num2).toString();
+        break;
+      case '-':
+        this.resultado = (num1 - num2).toString();
+        break;
+      case '*':
+        this.resultado = (num1 * num2).toString();
+        break;
+      case '/':
+        this.resultado = (num1 / num2).toString();
+        break;
+      default:
+        break;
+    }
+
+    this.pantalla = this.resultado;
+    this.operando1 = this.resultado;
+    this.operando2 = '';
+    this.operacion = '';
+  }
 }
